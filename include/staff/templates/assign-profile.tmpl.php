d177fa85a5b6952cde72b27895990742906e4ca5
Brian riviere <briviere@mixvoip.com>
2020-06-16 15:18:36 +0200

    te

--- include/staff/templates/assign-profile.tmpl.php
+++ include/staff/templates/assign-profile.tmpl.php
@@ -1,8 +1,9 @@
 <?php
 global $cfg;
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 if (!$info[':title'])
     $info[':title'] = __('Assign Profile');
 ?>
