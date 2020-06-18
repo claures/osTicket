7a43305a4ab6f2883fd5aac3329098f809c6fd23
Brian riviere <briviere@mixvoip.com>
2020-06-18 09:20:38 +0200

    te

--- include/ajax.tickets.php
+++ include/ajax.tickets.php
@@ -1429,7 +1429,8 @@ class TicketsAjaxAPI extends AjaxController
 				if($return['success']){
 					$sql = 'UPDATE ost_ticket__cdata SET profile_id = "'.$_POST['profile_id'].'" '
 					. ' WHERE ticket_id = "'.$tid.'" ';
-					if (($res = db_query($sql)))Http::response(200, 'updated and assigned');
+					$sql2 = "UPDATE `ost_form_entry_values` SET `value`= '".$_POST['profile_id']."' WHERE entry_id = $tid AND field_id = 53";
+					if (($res = db_query($sql)) && ($res = db_query($sql2)))Http::response(200, 'updated and assigned');
 					else Http::response(200, 'Contact created and but not assigned to ticket');
 				}
 				else Http::response(500, 'not updated and nots assigned');
